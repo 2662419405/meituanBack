15ede8b74140bcc122099044f889c12e9ec3e985
孙航 <2662419405@qq.com>
2020-02-20 13:32:58 +0800

    :egg: 尝试使用start命令直接在服务器启动

--- pages/index.vue
+++ pages/index.vue
@@ -2,28 +2,6 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        meituanBack
-      </h1>
-      <h2 class="subtitle">
-        My first-rate Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
